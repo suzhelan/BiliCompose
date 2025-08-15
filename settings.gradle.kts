e325f63667c00a677cb552877f86aaa92c8e1f4c
suzhelan <suzhelan@gmail.com>
2025-08-15 16:42:58 +0800

    build(deps): compose-multiplatform 1.9.0-beta03

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,6 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google/") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
         gradlePluginPortal()
         mavenCentral()
         google()
@@ -16,12 +13,7 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         google()
-        maven { url = uri("https://maven.aliyun.com/repository/public/") }
-        maven { url = uri("https://maven.aliyun.com/repository/google/") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://jogamp.org/deployment/maven/") }
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 includeBuild("build-plugin")
