ed6ea258e5b40634f7c384b39ed851637b1d5ed3
suzhelan <sacz@163.com>
2025-09-12 19:35:02 +0800

    build(build-plugin): 更新仓库配置- 移除了 Aliyun Maven镜像仓库 -调整了仓库的加载顺序：Gradle Plugin Portal -> Maven Central -> Google -> JitPack

--- build-plugin/build.gradle.kts
+++ build-plugin/build.gradle.kts
@@ -5,13 +5,10 @@ plugins {
 }
 
 repositories {
-    maven { url = uri("https://maven.aliyun.com/repository/public/") }
-    maven { url = uri("https://maven.aliyun.com/repository/google/") }
-    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
-    maven { url = uri("https://jitpack.io") }
-    google()
     gradlePluginPortal()
     mavenCentral()
+    google()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
