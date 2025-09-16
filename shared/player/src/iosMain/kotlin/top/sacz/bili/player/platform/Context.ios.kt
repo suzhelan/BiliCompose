15cf68abcbd1b11e238faf58fe243c347b15940a
suzhelan <suzhelan@gmail.com>
2025-09-16 15:33:13 +0800

    feat(player):添加平台Context

---
+++ shared/player/src/iosMain/kotlin/top/sacz/bili/player/platform/Context.ios.kt
@@ -0,0 +1,8 @@
+package top.sacz.bili.player.platform
+
+import androidx.compose.runtime.ProvidableCompositionLocal
+
+actual val BiliLocalContext: ProvidableCompositionLocal<BiliContext>
+    get() = TODO("Not yet implemented")
+
+actual abstract class BiliContext
