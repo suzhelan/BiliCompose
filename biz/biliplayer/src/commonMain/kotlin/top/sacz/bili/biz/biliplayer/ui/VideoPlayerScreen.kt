6add84552e7399fc187d75d135d3d4e38c8c80fd
suzhelan <sacz@163.com>
2025-09-20 20:17:50 +0800

    feat(BiliPlayer):尝试使用SupportingPaneScaffold优化大屏(Desktop)体验

--- biz/biliplayer/src/commonMain/kotlin/top/sacz/bili/biz/biliplayer/ui/VideoPlayerScreen.kt
+++ biz/biliplayer/src/commonMain/kotlin/top/sacz/bili/biz/biliplayer/ui/VideoPlayerScreen.kt
@@ -3,6 +3,10 @@ package top.sacz.bili.biz.biliplayer.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.PaneAdaptedValue
+import androidx.compose.material3.adaptive.layout.SupportingPaneScaffold
+import androidx.compose.material3.adaptive.layout.SupportingPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.rememberSupportingPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -38,26 +42,35 @@ private fun PlayerUI(
     playerParams: PlayerParams,
     viewModel: VideoPlayerViewModel,
 ) {
-    /*val scaffoldNavigator = rememberSupportingPaneScaffoldNavigator()
+    val scaffoldNavigator = rememberSupportingPaneScaffoldNavigator(
+
+    )
 
     SupportingPaneScaffold(
         directive = scaffoldNavigator.scaffoldDirective,
         scaffoldState = scaffoldNavigator.scaffoldState,
         mainPane = {
-
+            if (scaffoldNavigator.scaffoldValue[SupportingPaneScaffoldRole.Supporting] == PaneAdaptedValue.Hidden) {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    //播放器
+                    MediaUI(playerParams, viewModel)
+                    VideoInfoUI(playerParams, viewModel)
+                }
+            } else {
+                MediaUI(playerParams, viewModel)
+            }
         },
         supportingPane = {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                VideoInfoUI(playerParams, viewModel)
+            }
         },
         extraPane = {},
     )
-    */
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        //播放器
-        MediaUI(playerParams, viewModel)
-        VideoInfoUI(playerParams, viewModel)
-    }
 
 }
 
