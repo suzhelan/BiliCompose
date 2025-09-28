ed766d9fb1c9ea52565b8e4f267d4f87e5f27f14
suzhelan <sacz@163.com>
2025-09-28 19:18:55 +0800

    fix(player):正式解决desktop无法实现音视频流分离同步的问题

--- shared/player/src/desktopMain/kotlin/top/suzhelan/bili/player/controller/PlayerSyncController.desktop.kt
+++ shared/player/src/desktopMain/kotlin/top/suzhelan/bili/player/controller/PlayerSyncController.desktop.kt
@@ -19,20 +19,10 @@ actual object PlayerMediaDataUtils {
                 //音频流合并
                 add("input-slave=$audioUrl")
                 //请求头
-                /*add("http-user-agent=${PlayerSyncController.headers["User-Agent"]}")
-                add("http-referrer=${PlayerSyncController.headers["Referer"]}")*/
-                for ((key, value) in PlayerSyncController.headers) {
-                    add("http-header=$key:$value")
-                }
+                add("http-user-agent=${PlayerSyncController.headers["User-Agent"]}")
+                add("http-referrer=${PlayerSyncController.headers["Referer"]}")
             }.toTypedArray()
         )
-        /*vlcPlayer.media().prepare(
-            videoUrl,
-            "input-slave=$audioUrl",
-            ":network-caching=1000",       // 缓冲1秒减少抖动
-            ":clock-jitter=0",             // 关闭时钟抖动补偿
-            ":clock-synchronization=exact" // 强制精确同步模式
-        )*/
     }
 
     actual fun setFullScreen(context: BiliContext, fullScreen: Boolean) {
